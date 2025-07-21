431e960a8fcc139269aedc9763d22f47182f119d
thakoryashraj <thakoryashraj14@gmail.com>
2025-07-21 07:49:46 +0530

    Fix ProtectedRoute Import Error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { Toaster } from './components/ui/sonner';
-import ProtectedRoute from './components/ProtectedRoute';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import Login from './pages/Login';
 import Chat from './pages/Chat';
 import Leads from './pages/Leads';
