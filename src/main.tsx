fae9b045fc7a7c0830c6727dff3a56adf6694b83
Yashraj <yashrajthakor142@gmail.com>
2025-07-21 17:56:58 +0530

    Revert "Fix Preview Issues"
    This reverts commit 967d547f6da7b7f7bab88f4a47c6e95213c30aab.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
-
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
